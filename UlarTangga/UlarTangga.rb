c33e0e1eeb6d7a296b2460ed97fafde4686293cf
ghanifaza <fazaghania@yahoo.com>
2015-11-18 14:45:27 +0700

    Update UlarTangga.rb

--- UlarTangga/UlarTangga.rb
+++ UlarTangga/UlarTangga.rb
@@ -1,1 +1,27 @@
-
+class Ladder
+	@num
+	@placed
+	@posInBoard
+	
+	def initialize
+		num = 10
+		placed = false
+		posInBoard = 0
+	end
+	
+	def setPosInBoard(pos)
+		posInBoard = pos
+	end
+	
+	def getPosInBoard
+		return posInBoard
+	end
+	
+	def setPlaced(placed)
+		placed = placed
+	end
+	
+	def getPlaced
+		return placed
+	end
+end
