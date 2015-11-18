b276993bbcadb84bb3f31b80e0b63eda0f71a21d
karyotubruk <irfan_teuing@yahoo.com>
2015-11-18 15:44:35 +0700

    Update UlarTangga.rb

--- UlarTangga/UlarTangga.rb
+++ UlarTangga/UlarTangga.rb
@@ -1,1 +1,46 @@
+class Floor 
+    @num
+    @empty
+    @snake
+    @ladder
 
+    def initialize(num,empty, snake, ladder)
+        @num=num
+        @empty=empty
+        @snake=snake
+        @ladder=ladder
+    end
+
+    def setNum(num)
+        @num=num
+    end
+
+    def setSnake(snake)
+        @snake=snake
+    end
+
+    def setLadder(ladder)
+        @ladder=ladder
+    end
+
+    def isEmpty()
+        return @empty
+    end
+
+    def setEmpty(empty)
+        @empty=empty
+    end
+
+    def getNum()
+        return @num
+    end
+
+    def isSnake()
+        return @snake
+    end
+
+    def isLadder()
+        return @ladder
+    end
+
+end
