421b1e1647ce25db7c20f7e3b06c98aa3ce6dbdf
calvintorsa <7314076@student.unpar.ac.id>
2015-11-24 14:58:11 +0700

    Update Player.rb

--- UlarTangga/Player.rb
+++ UlarTangga/Player.rb
@@ -1,118 +1,47 @@
-class Player 
-    @position
-    
-    def initialize()
-    
-      @position = Floor(1).new
+class Player
+  @position
+  
+  def initialize
+    @position = Floor.new(1)
+  end
+  
+  def rollDice
+    return Random.rand(6)+1
+  end
+  
+  def move
+    a = rollDice
+    if @position.getNum == 1
+      @position.setNum(a)
+      return a
+    elsif @position.getNum+a>100
+      b = @position.getNum+a
+      @position.setNum(100-(b-100))
+      return a
+    else
+      @position.setNum(@position.getNum+a)
+      return a
     end
-    
-    def rollDice()
-    
-      return Random.new(6)+1
+  end
+  
+  def  gotSnake
+    if @position.isSnake
+      @position.setNum (@position.getNum-10)
     end
-    def move()
-    
-      a = rollDice()
-      if @position.getNum()==1
-        
-        @position.setNum(a)
-        return a
-        
-        elsif @position.getNum()+a>100
-        
-          b = @position.getNum()+a
-          @position.setNum(100-(b-100))
-          return a
-        
-        elsif
-        
-          @position.setNum(@position.getNum()+a)
-          return a
-        
-      end
-    end
-    
-    def checkLad(ladder)
-      
-      for i in 0..ladder.size()
-          
-        if ladder.get(i).getPosInBoard()== @position.getNum()
-              
-          if @position.getNum()>=80 && @position.getNum()<90
-                  
-            @position.setNum(@position.getNum()+10)
-            print "Kamu naik tangga!"
-            break
-                  
-            elsif
-                  
-            rand = Random.new(31)+10
-            if @position.getNum()+rand>100
-                      
-              @position.setNum(@position.getNum()+10)
-                puts "Kamu naik tangga!"
-                break
-                        
-                elsif
-                        
-                @position.setNum(@position.getNum()+rand)
-                puts "Kamu naik tangga!"
-                break
-              end
-            end
-        end
-      end
-    end
-    
-    def checkSnake(snake)
-      
-      for i in 0..snake.size()
-        if snake.get(i).getPosInBoard()== @position.getNum()
-            
-                if @position.getNum()<20 && @position.getNum()>=10
-                  
-            @position.setNum(@position.getNum()-10)
-                  print "Kamu terkena Ular!"
-            break
-                  
-            elsif
-                  
-            rand = Random.new(31)+10
-            if @position.getNum()-rand>0
-                      
-              @position.setNum(@position.getNum()+10)
-              puts "Kamu terkena Ular!"
-              break
-            end
-          end
-        end
-      end
-    end
-                
-    def  gotSnake()
+  end
 
-        if @position.isSnake()
 
-            @position.setNum (@position.getNum()-10)
-          
-        end
+  def gotLadder
+    if @position.isLadder
+      @position.setNum (@position.getNum+10)
     end
+  end
 
-    
-    def gotLadder()
-
-        if @position.isLadder()
-
-            @position.setNum (@position.getNum()+10)
-          
-        end
-    end
-          
-    def getNowPos()
-
-        return @position.getNum();
-    end
-
+  def getNowPos
+    return @position.getNum
+  end
+  
+  def setNowPos(angka)
+    @position.setNum(angka)
+  end
 end
-
-        
